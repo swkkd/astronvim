96a293990697c88de4c94c3a0582d1a72ee2b033
aleksandr dzikajev <swkkd@swkPC>
2023-12-03 00:36:11 +0200

    v0.5

---
+++ lsp/config/lua_ls.lua
@@ -0,0 +1,25 @@
+return {
+  settings = {
+    Lua = {
+      format = {
+        enable = false,
+      },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable",
+      },
+      workspace = {
+        checkThirdParty = false,
+      },
+      diagnostics = {
+        disable = { "lowercase-global" },
+      },
+      codelens = {
+        enable = true,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
